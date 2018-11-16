77670b0ddbf2a0f6d4155d614fec4286ecc39ab7
PainKiller_Ninad <ninadpatil100@gmail.com>
2018-11-16 09:44:23 +0530

    Added Pie

--- source.sh
+++ source.sh
@@ -7,14 +7,17 @@ case $flavour in
 	1)
 	   printf "Syncing all the necessary trees for building Oreo.\n"
 	   git clone https://github.com/PainKiller3/device_asus_msm8916-common.git device/asus/msm8916-common -b 8.1
-     git clone https://github.com/PainKiller3/device_asus_Z010D device/asus/Z010D -b 8.1
-     git clone https://github.com/PainKiller3/vendor_Z010D.git vendor/asus -b 8.1
-     git clone https://github.com/ShadowReaper1/android_kernel_asus_msm8916-1 kernel/asus/msm8916 -b lineage-15.1-2
+    	   git clone https://github.com/PainKiller3/device_asus_Z010D device/asus/Z010D -b 8.1
+    	   git clone https://github.com/PainKiller3/vendor_Z010D.git vendor/asus -b 8.1
+     	   git clone https://github.com/ShadowReaper1/android_kernel_asus_msm8916-1 kernel/asus/msm8916 -b lineage-15.1-2
 	   printf "\nSource Synned."
 	   ;;
 	2)
            printf "Syncing all the necessary trees for building Pie.\n"
-           printf "\nComing Soon"
+           git clone https://github.com/PainKiller3/device_asus_msm8916-common.git device/asus/msm8916-common -b 9
+    	   git clone https://github.com/PainKiller3/device_asus_Z010D device/asus/Z010D -b 9
+    	   git clone https://github.com/PainKiller3/vendor_Z010D.git vendor/asus -b 9
+     	   git clone https://github.com/ShadowReaper1/android_kernel_asus_msm8916-1 kernel/asus/msm8916 -b lineage-16.0
            printf "Source Synned."
            ;;
         
