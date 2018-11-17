9e8e5ae558fdebac08f2371eb5ff120da6f5fc20
PainKiller_Ninad <ninadpatil100@gmail.com>
2018-11-17 12:59:03 +0530

    Added Rom repo and ccache

--- setup.sh
+++ setup.sh
@@ -7,5 +7,12 @@ sudo apt-get install make
 sudo apt-get install bison
 sudo apt-get install bc
 sudo apt-get install gcc
+sudo apt-get install ccache
 git clone https://github.com/akhilnarang/scripts.git scripts
 bash setup/android_build_env.sh
+
+#Rom Ready Repo
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
