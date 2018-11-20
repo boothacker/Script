182f9d5d402c3776e3b2657e2e924c8056cd36a4
PainKiller_Ninad <ninadpatil100@gmail.com>
2018-11-21 00:37:27 +0530

    Update branch Pie

--- source.sh
+++ source.sh
@@ -14,10 +14,10 @@ case $flavour in
 	   ;;
 	2)
            printf "Syncing all the necessary trees for building Pie.\n"
-           git clone https://github.com/PainKiller3/device_asus_msm8916-common.git device/asus/msm8916-common -b 9
-    	   git clone https://github.com/PainKiller3/device_asus_Z010D device/asus/Z010D -b 9
-    	   git clone https://github.com/PainKiller3/vendor_Z010D.git vendor/asus -b 9
-     	   git clone https://github.com/ShadowReaper1/android_kernel_asus_msm8916-1 kernel/asus/msm8916 -b lineage-16.0
+           git clone https://github.com/PainKiller3/device_asus_msm8916-common.git device/asus/msm8916-common -b 9.0
+    	   git clone https://github.com/PainKiller3/device_asus_Z010D device/asus/Z010D -b 9.0
+    	   git clone https://github.com/PainKiller3/vendor_Z010D.git vendor/asus -b 9.0
+     	   git clone https://github.com/ShadowReaper1/android_kernel_asus_msm8916.git kernel/asus/msm8916 -b 1
            printf "Source Synned."
            ;;
         
