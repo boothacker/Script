f16e20ad8fa6503965bfdf4fcad16bd8c0cf67ec
PainKiller_Ninad <ninadpatil100@gmail.com>
2018-11-21 00:44:18 +0530

    Update

--- blitz.sh
+++ blitz.sh
@@ -3,8 +3,8 @@ export USE_CCACHE=1
 export CCACHE_DIR=/home/ninad/.ccache
 prebuilts/misc/linux-x86/ccache/ccache -M 100G
 . build/envsetup.sh
-lunch romname_Z010D-userdebug
-#lunch romname_aio_otfp-userdebug
+lunch bootleg_Z010D-userdebug
 export KBUILD_BUILD_USER=Ninad
 export KBUILD_BUILD_HOST=REIGNZ
-mka bacon -j32
+export ALLOW_MISSING_DEPENDENCIES=true
+make bacon -j$(nproc --all) | tee log.txt
