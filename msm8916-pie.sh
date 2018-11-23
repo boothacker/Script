d4c2effe6e746090fbcf6962d5c5341f6511b837
Ninad Patil (REIGNZ) <ninadpatil100@gmail.com>
2018-11-23 22:38:25 +0530

    FML -_-

--- msm8916-pie.sh
+++ msm8916-pie.sh
@@ -4,15 +4,21 @@ printf "Cloning Audio Hals!\n"
 cd hardware/qcom/audio-caf
 rm -rf msm8916
 git clone https://github.com/lettuce-pie/LOS-16-hals.git msm8916 -b audio-caf/msm8916
-
+cd .. && cd .. && cd ..
 
 printf "Cloning Media Hals!\n"
 cd hardware/qcom/media-caf
 rm -rf msm8916
 git clone https://github.com/lettuce-pie/LOS-16-hals.git msm8916 -b media-caf/msm8916
-
+cd .. && cd .. && cd ..
 
 printf "Cloning Display Hals!\n"
 cd hardware/qcom/display-caf
 rm -rf msm8916
 git clone https://github.com/lettuce-pie/LOS-16-hals.git msm8916 -b display-caf/msm8916
+cd .. && cd .. && cd ..
+
+printf "Cloning Audio Vendor!\n"
+rm -rf vendor/qcom/opensource/audio
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audio.git vendor/qcom/opensource/audio -b lineage-16.0
+cd .. && cd .. && cd ..
